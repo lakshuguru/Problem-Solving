95ff3c15f8f352624e914b4a8ee573e5c888217f
LAKSHANAA G S <77288889+lakshuguru@users.noreply.github.com>
2023-01-23 21:52:45 +0530

    new

--- C-PROGS/practice code/palindrome.c
+++ C-PROGS/practice code/palindrome.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+/*
 void main(){
     char s[]="madam",f[5],b[5];
     int c=0;
@@ -22,3 +22,28 @@ void main(){
     }
     
 }
+*/
+/*
+void main(){
+    int x=121;
+    int m=x,c=0,z=0;
+    while(m!=0){
+        if(x>0){
+            z=z*10+m%10;
+            m=m/10;
+        }
+        else{
+            z=z*10-m%10;
+            m=m/10;
+        }
+    }
+   
+    //printf("\n%d\t%d",x,z);
+    if(z==x){
+        printf("true");
+    }
+    else{
+        printf("false");
+    }
+}
+*/
