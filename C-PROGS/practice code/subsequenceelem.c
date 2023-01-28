e3152fb8a2b906b859fdb61029f0a5253ca95ead
LAKSHANAA G S <77288889+lakshuguru@users.noreply.github.com>
2023-01-28 09:57:18 +0530

    subsequence

--- C-PROGS/practice code/subsequenceelem.c
+++ C-PROGS/practice code/subsequenceelem.c
@@ -8,12 +8,12 @@ explanation: a sub-sequence 1 2 3 exist.
 
 #include<stdio.h>
 void main(){
-    int n=3,a[]={1,2,3},i,j,k;
+    int n=3,a[]={1,2,1,1,3},i,j,k;
     for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
+        for(j=i+1;j<n;){
             for(k=j+1;k<n;k++){
                 //printf("\n----> %d",k);
-                if((a[i]<a[j]<a[k])){
+                if((a[i]<a[j]<a[k])&&i < j < k){
                     printf("\ni:%d j:%d k:%d",i,j,k);
                     printf("\n%d\t%d\t%d",a[i],a[j],a[k]);
                     break;
@@ -24,3 +24,4 @@ void main(){
     }
 
 }
+
