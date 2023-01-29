76f7752074a57e743043b6fb44b8065bc659d992
LAKSHANAA G S <77288889+lakshuguru@users.noreply.github.com>
2023-01-29 23:31:38 +0530

    trainning

---
+++ channel-btect/predicttheoutput.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/*
+void main(){
+    int a[]={9,5,2,4,7};
+    int *p;
+    p=&a[2];
+    printf("%d\n",*p);
+    printf("%d\n"*(p+1));
+    printf("%d",*(p-1));
+}
+*/
+/*
+void ptr(char**);
+void main(){
+char *a[]={"abc","fgk","rty","tyu"};
+ptr(a);
+}
+void ptr(char **p){
+    char *t;
+    t=(p+=sizeof(int))[-2];   //t=p[4-1]---->t=p[3]
+    printf("%s\n",t);
+}
+*/
