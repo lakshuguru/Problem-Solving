2b9b895502d2d8637b29ef69673775237a3c8b2a
LAKSHANAA G S <77288889+lakshuguru@users.noreply.github.com>
2023-01-31 02:20:04 +0530

    ptr

--- channel-btect/predicttheoutput.c
+++ channel-btect/predicttheoutput.c
@@ -21,3 +21,87 @@ void ptr(char **p){
     printf("%s\n",t);
 }
 */
+/*
+void main(){
+    int a=130;    
+    char *p;
+    p=(char *)&a;     //cyclic rotation
+    printf("%d",*p);
+}
+*/
+/*
+void main(){
+    int i=3;
+    int *j;
+    int **k;
+    j=&i;
+    k=&j;
+    k++;
+    printf("%d",**k);
+}      
+*/
+/*
+void main(){
+    int i=3;
+    int *j;
+    j==&i;
+    j++;
+    printf("%d",*j);
+}
+*/
+/*
+void main(){
+    char *p="hello";
+    char a[22];
+    strcpy(a,"world");
+    printf("\n%s%s ",p,a);
+}
+*/
+/*
+void main(){
+    char *p="hello";
+    char a[22];
+    *p="world";
+    printf("\n%s%s ",p,a);
+}
+*/
+/*
+void main(){
+    char *p="helloworld";
+    pritf("%s",p+4);
+}
+*/
+/*
+void main(){
+    char *p="campus";
+    printf("%c\n",*&*p);
+}
+*/
+/*
+void main(){
+    register a=1;
+    int far *p;
+    p=&a;
+    printf("%u",p);
+}
+*/
+/*
+void main(){
+    char a=30, b=5;
+    char *p=&a, *q=&b;
+    printf("%d",p-q);         //when we subtract same datatype the result is always 1
+}
+*/
+/*
+unsigned long int(*function())[5]{
+    static unsigned long int a[5]={2,3,5,7,11};
+    printf("%d",*a)
+    return &a;
+}
+void main(){
+    unsigned long int(*p)[5];
+    p=function();
+    printf("%d",*(*p+4));
+}
+*/
+
